4bfa64020abe614b0fd028465a51027164c5f65e
Prashant Kumar Jha <prashant@prashants-air.home>
2023-05-30 14:48:28 -0400

    commiting to Dev branch

---
+++ Excercise2.py
@@ -0,0 +1,17 @@
+# Python script to calculate the factorial of a number using recursion
+
+# Function to calculate factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Get user input for the number
+num = int(input("Enter a number: "))
+
+# Calculate the factorial using the function
+result = factorial(num)
+
+# Print the result
+print("The factorial of", num, "is:", result)
